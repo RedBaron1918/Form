49e0dca041ab2323a4b59141b0ea87b83b14d533
RedBaron1918 <asatianikotiko1@gmail.com>
2022-09-21 22:39:08 +0400

    adding form in table

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,18 @@
     <form class="row g-4" @submit.prevent="SubmitForm">
       <InputStyle class="col-md-5">
         <NormalInput
-          v-model="FirstName"
+          v-model="FormValues.FirstName"
           type="text"
           label="First name"
           :class="
-            v$.FirstName.$errors.length >= 1 ? 'border border-danger' : ''
+            v$.FormValues.FirstName.$errors.length >= 1
+              ? 'border border-danger'
+              : ''
           "
         />
         <span
           style="color: red"
-          v-for="error in v$.FirstName.$errors"
+          v-for="error in v$.FormValues.FirstName.$errors"
           :key="error.$uid"
         >
           {{ error.$message }}
@@ -20,14 +22,18 @@
       </InputStyle>
       <InputStyle class="col-md-5">
         <NormalInput
-          v-model="LastName"
+          v-model="FormValues.LastName"
           type="text"
           label="Last Name"
-          :class="v$.LastName.$errors.length >= 1 ? 'border border-danger' : ''"
+          :class="
+            v$.FormValues.LastName.$errors.length >= 1
+              ? 'border border-danger'
+              : ''
+          "
         />
         <span
           style="color: red"
-          v-for="error in v$.LastName.$errors"
+          v-for="error in v$.FormValues.LastName.$errors"
           :key="error.$uid"
         >
           {{ error.$message }}
@@ -36,14 +42,18 @@
 
       <InputStyle class="col-md-10">
         <NormalInput
-          v-model="Adress"
+          v-model="FormValues.Adress"
           type="text"
           label="Adress"
-          :class="v$.Adress.$errors.length >= 1 ? 'border border-danger' : ''"
+          :class="
+            v$.FormValues.Adress.$errors.length >= 1
+              ? 'border border-danger'
+              : ''
+          "
         />
         <span
           style="color: red"
-          v-for="error in v$.Adress.$errors"
+          v-for="error in v$.FormValues.Adress.$errors"
           :key="error.$uid"
         >
           {{ error.$message }}
@@ -51,19 +61,25 @@
       </InputStyle>
 
       <InputStyle class="col-md-3">
-        <NormalInput v-model="SelectedDate" type="date" label="Select Date" />
+        <NormalInput
+          v-model="FormValues.SelectedDate"
+          type="date"
+          label="Select Date"
+        />
       </InputStyle>
       <InputStyle class="col-md-3">
-        <Select v-model="Gender" label="Select Gender" />
+        <Select v-model="FormValues.Gender" label="Select Gender" />
       </InputStyle>
 
       <InputStyle class="col-10">
-        <TextArea v-model="Notes" label="Notes..." />
+        <TextArea v-model="FormValues.Notes" label="Notes..." />
       </InputStyle>
       <InputStyle class="col-3">
         <button type="submit" class="btn btn-success">Submit</button>
       </InputStyle>
     </form>
+
+    <Table :FormResult="FormResult" />
   </div>
 </template>
 
@@ -72,32 +88,41 @@ import NormalInput from "./components/normalInput.vue";
 import Select from "./components/Select.vue";
 import InputStyle from "./components/InputStyle.vue";
 import TextArea from "./components/TextArea.vue";
+import Table from "./components/Table.vue";
 import useVuelidate from "@vuelidate/core";
 import { required, maxLength } from "@vuelidate/validators";
 export default {
   name: "App",
+  setup() {
+    return { v$: useVuelidate() };
+  },
   components: {
     NormalInput,
     Select,
     InputStyle,
     TextArea,
+    Table,
   },
   data() {
     return {
-      v$: useVuelidate(),
-      FirstName: "",
-      LastName: "",
-      Adress: "",
-      SelectedDate: "",
-      Gender: "",
-      Notes: "",
+      FormValues: {
+        FirstName: "",
+        LastName: "",
+        Adress: "",
+        SelectedDate: "",
+        Gender: "",
+        Notes: "",
+      },
+      FormResult: [],
     };
   },
   validations() {
     return {
-      FirstName: { required },
-      LastName: { required },
-      Adress: { required, maxLengthValue: maxLength(35) },
+      FormValues: {
+        FirstName: { required },
+        LastName: { required },
+        Adress: { required, maxLengthValue: maxLength(35) },
+      },
     };
   },
 
@@ -105,9 +130,28 @@ export default {
     async SubmitForm() {
       const result = await this.v$.$validate();
       if (result) {
-        console.log(result);
+        this.FormResult.push({
+          FirstName: this.FormValues.FirstName,
+          LastName: this.FormValues.LastName,
+          Adress: this.FormValues.Adress,
+          SelectedDate: this.FormValues.SelectedDate,
+          Notes: this.FormValues.Notes,
+          Gender: this.FormValues.Gender,
+        });
+        console.log(this.FormResult, "first");
+        alert("sucsess");
+        this.Clear();
       } else {
+        alert("failed to submit");
       }
+    },
+    Clear() {
+      this.FormValues.FirstName = "";
+      this.FormValues.LastName = "";
+      this.FormValues.Adress = "";
+      this.FormValues.SelectedDate = "";
+      this.FormValues.Notes = "";
+      this.FormValues.Gender = "";
     },
   },
 };
---
+++ src/components/Table.vue
@@ -0,0 +1,32 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">Address</th>
+        <th scope="col">Date</th>
+        <th scope="col">Gender</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for:="(data, index) in FormResult">
+        <th scope="row">{{ index + 1 }}</th>
+        <td>{{ data.FirstName }}</td>
+        <td>{{ data.LastName }}</td>
+        <td>{{ data.Adress }}</td>
+        <td>{{ data.SelectedDate }}</td>
+        <td>{{ data.Gender }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: ["FormResult"],
+};
+</script>
+
+<style></style>
