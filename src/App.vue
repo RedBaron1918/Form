d1426400cf83d98631c84ce5ca2a8affd5e200cc
RedBaron1918 <asatianikotiko1@gmail.com>
2022-09-21 20:20:14 +0400

    added validations and submit button

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
   <div id="#app">
-    <form class="row g-4">
+    <form class="row g-4" @submit.prevent="SubmitForm">
       <InputStyle class="col-md-5">
         <NormalInput
           v-model="FirstName"
           type="text"
           label="First name"
-          required
+          :class="
+            v$.FirstName.$errors.length >= 1 ? 'border border-danger' : ''
+          "
         />
+        <span
+          style="color: red"
+          v-for="error in v$.FirstName.$errors"
+          :key="error.$uid"
+        >
+          {{ error.$message }}
+        </span>
       </InputStyle>
       <InputStyle class="col-md-5">
         <NormalInput
           v-model="LastName"
           type="text"
           label="Last Name"
-          required
+          :class="v$.LastName.$errors.length >= 1 ? 'border border-danger' : ''"
         />
+        <span
+          style="color: red"
+          v-for="error in v$.LastName.$errors"
+          :key="error.$uid"
+        >
+          {{ error.$message }}
+        </span>
       </InputStyle>
 
       <InputStyle class="col-md-10">
@@ -23,9 +39,15 @@
           v-model="Adress"
           type="text"
           label="Adress"
-          required
-          maxlength="35"
+          :class="v$.Adress.$errors.length >= 1 ? 'border border-danger' : ''"
         />
+        <span
+          style="color: red"
+          v-for="error in v$.Adress.$errors"
+          :key="error.$uid"
+        >
+          {{ error.$message }}
+        </span>
       </InputStyle>
 
       <InputStyle class="col-md-3">
@@ -38,6 +60,9 @@
       <InputStyle class="col-10">
         <TextArea v-model="Notes" label="Notes..." />
       </InputStyle>
+      <InputStyle class="col-3">
+        <button type="submit" class="btn btn-success">Submit</button>
+      </InputStyle>
     </form>
   </div>
 </template>
@@ -47,6 +72,8 @@ import NormalInput from "./components/normalInput.vue";
 import Select from "./components/Select.vue";
 import InputStyle from "./components/InputStyle.vue";
 import TextArea from "./components/TextArea.vue";
+import useVuelidate from "@vuelidate/core";
+import { required, maxLength } from "@vuelidate/validators";
 export default {
   name: "App",
   components: {
@@ -57,6 +84,7 @@ export default {
   },
   data() {
     return {
+      v$: useVuelidate(),
       FirstName: "",
       LastName: "",
       Adress: "",
@@ -64,6 +92,23 @@ export default {
       Gender: "",
       Notes: "",
     };
+  },
+  validations() {
+    return {
+      FirstName: { required },
+      LastName: { required },
+      Adress: { required, maxLengthValue: maxLength(35) },
+    };
+  },
+
+  methods: {
+    async SubmitForm() {
+      const result = await this.v$.$validate();
+      if (result) {
+        console.log(result);
+      } else {
+      }
+    },
   },
 };
 </script>
