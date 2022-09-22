2fa0525ae747540c10cf53e1d8505f6b3733bfd2
RedBaron1918 <asatianikotiko1@gmail.com>
2022-09-22 15:42:36 +0400

    fixed table responsivnes

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,41 +1,40 @@
 <template>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">First Name</th>
-        <th scope="col">Last Name</th>
-        <th scope="col">Address</th>
-        <th scope="col">Date</th>
-        <th scope="col">Gender</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for:="(data, index) in FormResult"
-        @click="popup(data.Notes)"
-        style="cursor: pointer"
-      >
-        <th scope="row">
-          {{ index + 1 }}
-        </th>
-        <td>{{ data.FirstName }}</td>
-        <td>{{ data.LastName }}</td>
-        <td>{{ data.Adress }}</td>
-        <td>{{ data.SelectedDate }}</td>
-        <td>{{ data.Gender }}</td>
-        <td>
-          <button
-            type="button"
-            class="btn btn-danger"
-            @click="$emit('RemoveItem', data)"
-          >
-            Danger
-          </button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table-responsive mt-2">
+    <table class="table align-middle">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Address</th>
+          <th scope="col">Date</th>
+          <th scope="col">Gender</th>
+          <th scope="col">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for:="(data, index) in FormResult" style="cursor: pointer">
+          <th scope="row" @click="popup(data.Notes)">
+            {{ index + 1 }}
+          </th>
+          <td @click="popup(data.Notes)">{{ data.FirstName }}</td>
+          <td @click="popup(data.Notes)">{{ data.LastName }}</td>
+          <td @click="popup(data.Notes)">{{ data.Adress }}</td>
+          <td @click="popup(data.Notes)">{{ data.SelectedDate }}</td>
+          <td @click="popup(data.Notes)">{{ data.Gender }}</td>
+          <td>
+            <button
+              type="button"
+              class="btn btn-danger"
+              @click="$emit('RemoveItem', data)"
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
